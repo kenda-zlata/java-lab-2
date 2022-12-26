b8afc83d665cd2676e459b260bff32780725616f
kenda-zlata <121447867+kenda-zlata@users.noreply.github.com>
2022-12-26 20:16:02 +0300

    initial commit

---
+++ src/bsu/rfe/java/group6/lab2/Kenda/varC1/MainApplication.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group6.lab2.Kenda.varC1;
+
+import javax.swing.JFrame;
+
+public class MainApplication
+{
+    public static void main(String[]args)
+    {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
